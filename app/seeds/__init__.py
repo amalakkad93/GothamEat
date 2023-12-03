9e012a8717fa1c878e362e0c4622ab3c8321821f
amala <amalakkad93@outlook.com>
2023-12-02 17:25:57 -0800

    Final stages for production part18

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -5,8 +5,8 @@ from .restaurant_seeder import seed_restaurants, undo_restaurants
 from .menu_item_seeder import seed_menu_items, undo_menu_items
 from .review_seeder import seed_reviews, undo_reviews
 from .review_img_seeder import seed_review_images, undo_review_images
-from .shopping_cart_seeder import seed_shopping_carts_and_items, undo_shopping_carts_and_items
-from .order_seeder import seed_orders_and_order_items, undo_orders_and_order_items
+# from .shopping_cart_seeder import seed_shopping_carts_and_items, undo_shopping_carts_and_items
+# from .order_seeder import seed_orders_and_order_items, undo_orders_and_order_items
 # from .payment_seeder import seed_payments, undo_payments
 from app.models.db import db, environment, SCHEMA
 
@@ -24,8 +24,8 @@ def seed():
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
 
-        undo_orders_and_order_items()
-        undo_shopping_carts_and_items()
+        # undo_orders_and_order_items()
+        # undo_shopping_carts_and_items()
         undo_review_images()
         undo_reviews()
         # undo_favorites()
@@ -39,16 +39,16 @@ def seed():
     # seed_favorites()
     seed_reviews()
     seed_review_images()
-    seed_shopping_carts_and_items()
-    seed_orders_and_order_items()
+    # seed_shopping_carts_and_items()
+    # seed_orders_and_order_items()
     # seed_payments()
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
     # undo_payments()
-    undo_orders_and_order_items()
-    undo_shopping_carts_and_items()
+    # undo_orders_and_order_items()
+    # undo_shopping_carts_and_items()
     undo_review_images()
     undo_reviews()
     # undo_favorites()
